9df0d7699d4337e9355dc4fb4dc20ce586a98f28
Sergey-Kosenko <serg.cos@mail.ru>
2021-09-18 06:55:35 +0500

    Start cloud file

---
+++ src/main/java/ru/kosenko/webcloudfile/WebcloudfileApplication.java
@@ -0,0 +1,13 @@
+package ru.kosenko.webcloudfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcloudfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebcloudfileApplication.class, args);
+	}
+
+}
